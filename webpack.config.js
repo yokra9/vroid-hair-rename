6d94e20c2926cc95f1bfff82ce42b6c7c79c1e47
yokra9 <53964890+yokra9@users.noreply.github.com>
2020-12-16 21:00:32 +0900

    Bundler Build Feature Flags

--- webpack.config.js
+++ webpack.config.js
@@ -32,5 +32,11 @@ module.exports = {
       },
     ],
   },
-  plugins: [new VueLoaderPlugin()],
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+  ],
 };
